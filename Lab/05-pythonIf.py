da9605bc1a41b5a0ec6f5836e2eba8f1c3ce3802
Krittipong Wachirangkul <34181035+newlesk@users.noreply.github.com>
2025-07-23 08:57:47 +0700

    up

---
+++ Lab/05-pythonIf.py
@@ -0,0 +1,8 @@
+age = input("Enter your age")
+age = int(age)
+
+
+if(age>=18 and age<50):
+    print("go")
+else:
+    print("home")
---
+++ Lab/08-pthonIf.py
@@ -0,0 +1,7 @@
+number = int(input("Enter your number"))
+
+print(number%2)
+if(number%2 == 0):
+    print("เลขคู่")
+else:
+    print("เลขคี่")
